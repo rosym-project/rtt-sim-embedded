e585b0b3641f3d3acea38c0ca3f6a07f65c8666f
xwavex <xwavedw@gmail.com>
2020-07-08 15:39:15 +0200

    server script is not spawning a robot anymore

--- scripts/launch_pybullet_server.py
+++ scripts/launch_pybullet_server.py
@@ -35,44 +35,9 @@ p.setRealTimeSimulation(0)
 # Chose step width
 p.setTimeStep(0.001) # TODO Chose the right number that matches with OROCOS RTT
 
-kukaUid = p.loadURDF("/home/dwigand/code/cogimon/CoSimA/pyBullet/catkin_py_ws/src/py-flex-assembly/gym_flexassembly/data/kuka-iiwa-7/model.urdf", useFixedBase=True)
-p.resetBasePositionAndOrientation(kukaUid, [0, -0.2, 0.5], [0,0,0,1])
-# numJoints = p.getNumJoints(kukaUid)
-# allJoint = []
-# zero = []
-# for jon in range(numJoints):
-#     allJoint.append(jon)
-#     zero.append(0.0)
-# p.setJointMotorControlArray(kukaUid, allJoint, p.VELOCITY_CONTROL, forces=zero)
-
-# while 1:
-#     # joint_pos_0 = p.readUserDebugParameter(dp_joint_pos_0)
-#     # joint_pos_1 = p.readUserDebugParameter(dp_joint_pos_1)
-#     # joint_pos_2 = p.readUserDebugParameter(dp_joint_pos_2)
-#     # joint_pos_3 = p.readUserDebugParameter(dp_joint_pos_3)
-#     # joint_pos_4 = p.readUserDebugParameter(dp_joint_pos_4)
-#     # joint_pos_5 = p.readUserDebugParameter(dp_joint_pos_5)
-#     # joint_pos_6 = p.readUserDebugParameter(dp_joint_pos_6)
-#     print("kukaUid = " + str(kukaUid))
-
-#     indicies = [1,2,3,4,5,6,7]
-
-#     print("numJoints = " + str(numJoints))
-#     print("indicies = " + str(len(indicies)))
-
-#     jointStates = p.getJointStates(kukaUid, indicies)
-#     q1=[]
-#     qdot1=[]
-#     zeroAccelerations=[]
-#     for i in range(len(indicies)):
-#         q1.append(jointStates[i][0])
-#         qdot1.append(jointStates[i][1])
-#         zeroAccelerations.append(0)
-#     print("Pos " + str(q1))
-#     print("Vel " + str(qdot1))
-
-#     time.sleep(1/1000) # TODO DLW
-
+# # # Use this is it is the responsibility of "pybullet" to spawn the robot
+# kukaUid = p.loadURDF("/home/dwigand/code/cogimon/CoSimA/pyBullet/catkin_py_ws/src/py-flex-assembly/gym_flexassembly/data/kuka-iiwa-7/model.urdf", useFixedBase=True)
+# p.resetBasePositionAndOrientation(kukaUid, [0, -0.2, 0.5], [0,0,0,1])
 
 try:
     print("Waiting for CTRL-C")
